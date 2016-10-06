da3a1676985ab0fe3d808f63c3e32b235e0dbc7f
luqinghi <luqhcwq@outlook.com>
2016-10-06 13:38:34 +0800

    1. 添加MapAPI文件夹。包括相关地图Web服务API，现在只有百度Place Suggestion API。 2. 添加MapAPIUserControl。包括自定义的地图相关控件。现在只有输入地址提示控件（利用百度服务）。

---
+++ MapUWP/MapAPI/BDMapAPI.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUCWQ.MapAPI
+{
+    public class BDMapAPI
+    {
+        private static string ak = "9SMLNEG9oTwVfcd0ciGBGKvSuv9kWne2";
+        private static string bdplaceapitemplate = "http://api.map.baidu.com/place/v2/suggestion?query={0}&region={1}&output=json&ak={2}";
+
+        public async static void GetSuggestPlaceByName(string inputname, string region, ObservableCollection<Result> suggestplace)
+        {
+            string reqstr = string.Format(bdplaceapitemplate, inputname, region, ak);
+            //string reqstrencode = WebUtility.UrlEncode(reqstr);
+            var http = new HttpClient();
+            var response = await http.GetAsync(reqstr);
+            var resultstr = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(ReturnResultByBD));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(resultstr));
+            var data = (ReturnResultByBD)serializer.ReadObject(ms);
+            suggestplace.Clear();
+            data.result.ForEach(p => suggestplace.Add(p));     
+        }
+    }
+
+
+    [DataContract]
+    public class Location
+    {
+        [DataMember]
+        public double lat { get; set; }
+        [DataMember]
+        public double lng { get; set; }
+
+    }
+    [DataContract]
+    public class Result
+    {
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public Location location { get; set; }
+        [DataMember]
+        public string uid { get; set; }
+        [DataMember]
+        public string city { get; set; }
+        [DataMember]
+        public string district { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+    [DataContract]
+    public class ReturnResultByBD
+    {
+        [DataMember]
+        public int status { get; set; }
+        [DataMember]
+        public string message { get; set; }
+        [DataMember]
+        public List<Result> result { get; set; }
+    }
+
+}
---
+++ MapUWP/MapAPIUserControl/BDSuggestionPlaceControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using LUCWQ.MapAPI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace LUCWQ.MapAPIUserControl
+{
+    public sealed partial class BDSuggestionPlaceControl : UserControl
+    {
+        //最后选择的地点
+        public Result SelectedSuggestion { get; set; } = new Result();
+
+        //建议地点列表
+        private ObservableCollection<Result> SuggestPlace = new ObservableCollection<Result>();
+        public BDSuggestionPlaceControl()
+        {
+            this.InitializeComponent();
+        }
+        private void MySuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            // 因为用户的输入而使得 Text 发生变化
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput || args.Reason == AutoSuggestionBoxTextChangeReason.SuggestionChosen)
+            {
+                BDMapAPI.GetSuggestPlaceByName(sender.Text, "全国", SuggestPlace);
+                MySuggestBox.ItemsSource = SuggestPlace;
+            }
+            // 通过代码使 Text 发生变化
+            //else if (args.Reason == AutoSuggestionBoxTextChangeReason.ProgrammaticChange)
+            //{
+            //}
+            // 因为用户在建议框（即下拉部分）选择了某一项而使得 Text 发生变化
+            //else if (args.Reason == AutoSuggestionBoxTextChangeReason.SuggestionChosen)
+            //{   
+            //}
+        }
+        private void MySuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (args.ChosenSuggestion != null)
+            {
+                SelectedSuggestion = (Result)args.ChosenSuggestion;
+            }
+            else
+            {
+                SelectedSuggestion.name = MySuggestBox.Text;
+            }
+        }
+
+        private void MySuggestBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(SelectedSuggestion.name))
+            {
+                SelectedSuggestion.name = MySuggestBox.Text;
+            }
+        }
+    }
+}
